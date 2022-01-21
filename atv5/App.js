90b2370cf9dbb90bf84c069c339e03edd8f66b15
AlexMcar <alex19mene@gmail.com>
2022-01-21 19:08:17 -0300

    atividade 5

---
+++ atv5/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import MainStackScreen from './src/components/CRUD/Routes';
+
+export default function App() {
+
+    return (
+      <SafeAreaProvider>
+      
+      <MainStackScreen></MainStackScreen>
+      
+      </SafeAreaProvider>
+  );
+}
+
